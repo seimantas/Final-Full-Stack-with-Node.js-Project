f4ea41b1cde102a5e05bce2b8019b7eb198f573d
seimantas <sipas.eimantas@gmail.com>
2023-03-21 21:46:06 +0200

    typescript setup

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,3 +1,14 @@
 import express from "express";
+import { LOCAL_PORT } from "./src/config";
+import { MongoClient } from "mongodb";
+
+const UIR = "mongodb://localhost:27017";
+const client = new MongoClient(UIR);
 
 const app = express();
+
+app.use(express.json());
+
+app.listen(LOCAL_PORT, () => {
+  console.log(`Server running on port ${LOCAL_PORT}`);
+});
---
+++ back-end/src/config.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const LOCAL_PORT = process.env.LOCAL_PORT;
